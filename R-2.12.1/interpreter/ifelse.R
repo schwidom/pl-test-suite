e8d24d7d74f8b893473bf321b0809e91fc04fb9d
Frank Schwidom <schwidom@gmx.net>
2013-05-07 21:55:14 +0200

    +

--- R-2.12.1/interpreter/ifelse.R
+++ R-2.12.1/interpreter/ifelse.R
@@ -7,6 +7,10 @@ te$checkComplainAndAdjustExpected( 0)
 
 te$test( all( c( 4, 5, 4) == ifelse( c( 1, 0, 1), 4, 5)))
 
+te$test( all( c( 1, 4, 3, 2) == ifelse( c( 1, 0, 0, 1), c( 1, 2), c( 3, 4))))
+
+te$test( all( c( 1, 2, 1, 3, 4, 5) == ifelse( c( 1, 1, 1, 0, 0, 0), c( 1, 2), c( 3, 4, 5))))
+
 te$checkComplainAndAdjustExpected( 1)
 
 invisible( {
@@ -17,17 +21,17 @@ invisible( {
 te$checkComplainAndAdjustExpected( 2)
 
 invisible( {
- ifelse( c( TRUE, TRUE), c( te$test( TRUE), te$test( TRUE)), c( te$test( FALSE), te$test( FALSE)))
- ifelse( c( FALSE, FALSE), c( te$test( FALSE), te$test( FALSE)), c( te$test( TRUE), te$test( TRUE)))
+ ifelse( c( TRUE, TRUE), te$test( TRUE), te$test( FALSE))
+ ifelse( c( FALSE, FALSE), te$test( FALSE), te$test( TRUE))
 })
 
-te$checkComplainAndAdjustExpected( 4)
+te$checkComplainAndAdjustExpected( 2)
 
 invisible(
- ifelse( c( TRUE, FALSE), c( te$test( TRUE), te$test( TRUE)), c( te$test( TRUE), te$test( TRUE)))
+ ifelse( c( TRUE, FALSE), te$test( TRUE), te$test( TRUE))
 )
 
-te$checkComplainAndAdjustExpected( 4)
+te$checkComplainAndAdjustExpected( 2)
 
 # see also:
 
