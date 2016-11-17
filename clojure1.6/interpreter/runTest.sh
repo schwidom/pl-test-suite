f793e4c6340d3d746b8024a4b9d43fc93221b975
Frank Schwidom <schwidom@gmx.net>
2016-11-17 21:28:47 +0100

    +

--- clojure1.6/interpreter/runTest.sh
+++ clojure1.6/interpreter/runTest.sh
@@ -4,6 +4,8 @@ test $# -eq 1 || { echo "usage: $0 <file>.c, exiting"; exit 1;}
 
 FILE="${1}"
 
+test "$( basename "$FILE" .clj).clj" == "$( basename "$FILE")" || { echo "wrong filename extension"; exit 1;}
+
 test "$( realpath "$FILE" )" == "$( realpath "testenv.clj" )" && exit 0
 test "$( realpath "$FILE" )" == "$( realpath "testdata.clj" )" && exit 0
 
---
+++ clojure1.6/interpreter/simple-types.clj
@@ -0,0 +1,43 @@
+
+(ns skeleton
+ (:use testenv)
+)
+
+(checkComplainAndAdjustExpected 0)
+
+(teTest true)
+(teTest (identity true))
+(teTest (not false))
+(teTest (not (not true)))
+
+(teTest (= java.lang.Long (type 1)))
+(teTest (= java.lang.Class (type (type 1))))
+(teTest (= java.lang.Class (type java.lang.Long)))
+
+(teTest (integer? 1))
+(teTest (rational? 1))
+(teTest (rational? 1/2))
+(teTest (not (integer? 1/2)))
+(teTest (identity (integer? (* 1/2 2/1) )))
+
+(teTest (float? 1.1))
+(teTest (not (float? (rationalize 1.1))))
+(teTest (identity (rational? (rationalize 1.1))))
+
+(teTest (= "1" (str 1)))
+(teTest (= "1.1" (str 1.1)))
+(teTest (= "1/2" (str 1/2)))
+(teTest (= 1 (long 1.1)))
+(teTest (= 1. (float 1)))
+(teTest (not (= 1 (float 1))))
+
+(teTest (= 1 (Integer. "1")))
+(teTest (= 1. (Double. "1")))
+
+(teTest (= java.lang.Double (type 1.1)))
+(teTest (= clojure.lang.Ratio (type 1/2)))
+(teTest (= java.lang.String (type "1")))
+
+(checkComplainAndAdjustExpected 26)
+
+(println (teResult))
