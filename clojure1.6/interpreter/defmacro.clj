880a8cb9546ec35b32393c1f7402273e27696d40
Frank Schwidom <schwidom@gmx.net>
2016-11-18 15:28:20 +0100

    +

---
+++ clojure1.6/interpreter/defmacro.clj
@@ -0,0 +1,71 @@
+
+
+(ns defmacro-test
+ (:use testenv)
+)
+
+(checkComplainAndAdjustExpected 0)
+
+(defmacro m1 [])
+
+(teTest (= nil (m1)))
+
+(defmacro m2 [a] a)
+
+(teTest (= 1 (m2 1)))
+
+(defmacro m3 [a b] (+ a b))
+
+(teTest (= 5 (m3 2 3)))
+
+(defmacro m4 [a b] `(+ ~a ~b))
+
+(teTest (= 5 (m4 2 3)))
+
+(teTest (= '(clojure.core/+ 2 3) (macroexpand '(m4 2 3))))
+
+(teTest (= 5 (eval (macroexpand '(m4 2 3)))))
+
+(defmacro m5 [a b] `(identity ~(+ a b)))
+
+(teTest (= 5 (m5 2 3)))
+
+(teTest (= '(clojure.core/identity 5) (macroexpand '(m5 2 3))))
+
+(teTest (= 5 (eval (macroexpand '(m5 2 3)))))
+
+(defmacro m6 [a b] `(identity ~(identity `(+ ~a ~b))))
+
+(teTest (= 5 (m6 2 3)))
+
+(teTest (= '(clojure.core/identity (clojure.core/+ 2 3)) (macroexpand '(m6 2 3))))
+
+(teTest (= 5 (eval (macroexpand '(m6 2 3)))))
+
+
+(defmacro add_a_b [] `(+ a b))
+(def add_a_b_code '(+ a b))
+
+(teTest (= '(clojure.core/+ defmacro-test/a defmacro-test/b) (macroexpand '(add_a_b))))
+
+; (teTest (= 5 (let [a 2 b 3] (+ a b))))
+; (teTest (= 5 (let [a 2 b 3] (eval add_a_b_code)))) ; Unable to resolve symbol: a in this context
+; (teTest (= 5 (let [a 2 b 3] (add_a_b)))) ; No such var: defmacro-test/a
+; (teTest (= 5 (binding [a 2 b 3] (eval add_a_b_code)))) ; Unable to resolve var: a in this context
+; (teTest (= 5 (let [a 2 b 3] (add_a_b)))) ; clojure.lang.Var$Unbound cannot be cast to java.lang.Number
+
+(declare ^:dynamic a)
+(declare ^:dynamic b)
+
+; (teTest (= 5 (let [a 2 b 3] (+ a b))))
+; (teTest (= 5 (let [a 2 b 3] (eval add_a_b_code)))) ; clojure.lang.Var$Unbound cannot be cast to java.lang.Number
+; (teTest (= 5 (let [a 2 b 3] (add_a_b)))) ; clojure.lang.Var$Unbound cannot be cast to java.lang.Number
+(teTest (= 5 (binding [a 2 b 3] (eval add_a_b_code)))) ; works
+; (teTest (= 5 (let [a 2 b 3] (add_a_b)))) ; clojure.lang.Var$Unbound cannot be cast to java.lang.Number
+
+(checkComplainAndAdjustExpected 14)
+
+(println (teResult))
+
+; see also: defmacro.clj macroexpand.clj macroexpand-1.clj eval.clj let.clj binding.clj declare.clj 
+
--- clojure1.6/interpreter/doc.clj
+++ clojure1.6/interpreter/doc.clj
@@ -7,6 +7,16 @@
 
 (checkComplainAndAdjustExpected 0)
 
+(def var_1 "docstring variable var_1" 1)
+
+(teTest (= (with-out-str (doc var_1))
+"-------------------------
+doc/var_1
+  docstring variable var_1
+"
+))
+
+
 (defn f1 "docstring function f1" 
  {} ; tags
  ([] nil)
@@ -19,16 +29,19 @@ doc/f1
   docstring function f1
 "))
 
-(def var_1 "docstring variable var_1" 1)
 
-(teTest (= (with-out-str (doc var_1))
+(defmacro m1 "docstring macro f1" [])
+
+(teTest (= (with-out-str (doc m1)) 
 "-------------------------
-doc/var_1
-  docstring variable var_1
-"
-))
+doc/m1
+([])
+Macro
+  docstring macro f1
+"))
 
-(checkComplainAndAdjustExpected 2)
+
+(checkComplainAndAdjustExpected 3)
 
 (println (teResult))
 
