8087558a2c5aa230a5755c9e46ea2fdd846f79e0
Frank Schwidom <schwidom@gmx.net>
2016-11-18 19:36:13 +0100

    +

--- clojure1.6/interpreter/topic-polymorphism.clj
+++ clojure1.6/interpreter/topic-polymorphism.clj
@@ -27,22 +27,32 @@
 ; (defmethod f3 [1 2] [a b] (+ a b))
 
 (defmulti f3 (fn [x] [(:k x)]))
-(defmethod f3 [:a] [key] 2)
-(defmethod f3 [:b] [key] 3)
+(defmethod f3 [:a] [key] ["case1" key])
+(defmethod f3 [:b] [key] ["case2" key])
 
-(teTest (= 2 (f3 {:k :a})))
-(teTest (= 3 (f3 {:k :b})))
+(teTest (= ["case1" {:k :a}] (f3 {:k :a})))
+(teTest (= ["case2" {:k :b}] (f3 {:k :b})))
 
 
 (defmulti f4 (fn [x y] [(:k1 x) (:k2 y)]))
-(defmethod f4 [:a :a] [key1 key2] (str "case1 " key1 " " key2))
+(defmethod f4 [:a :a] [key1 key2] ["case1" key1 key2])
 
-(teTest (= "case1 {:k1 :a} {:k2 :a}" (f4 {:k1 :a} {:k2 :a})))
-(teTest (= "case1 {:kx :x, :k1 :a} {:k2 :a, :ky :y}" (f4 {:k1 :a :kx :x} {:k2 :a :ky :y})))
-; (teTest (= nil (f4 {:k1 :b} {:k2 :a}))) ; No method in multimethod 'f4' for dispatch value: [:b :a]
-; (teTest (= nil (f4 {:k3 :a} {:k2 :a}))) ; No method in multimethod 'f4' for dispatch value: [nil :a]
+(teTest (= ["case1" {:k1 :a} {:k2 :a}] (f4 {:k1 :a} {:k2 :a})))
+(teTest (= ["case1" {:k1 :a :kx :x} {:k2 :a :ky :y}] (f4 {:k1 :a :kx :x} {:k2 :a :ky :y})))
 
-(checkComplainAndAdjustExpected 8)
+
+(defmulti f5 (fn [x] [(:k1 x) (:k2 x)]))
+(defmethod f5 [:a :a] [key] [ "case1" key ])
+(defmethod f5 [:a :b] [key] [ "case2" key ])
+(defmethod f5 :default [key] [ "case3" key ])
+(defmethod f5 [:a nil] [key] [ "case4" key ])
+
+(teTest (= ["case1" {:k1 :a, :k2 :a}] (f5 {:k1 :a :k2 :a})))
+(teTest (= ["case2" {:k1 :a, :k2 :b}] (f5 {:k1 :a :k2 :b})))
+(teTest (= ["case3" {:k1 :b, :k2 :b}] (f5 {:k1 :b :k2 :b})))
+(teTest (= ["case4" {:k1 :a, :kX :b}] (f5 {:k1 :a :kX :b})))
+
+(checkComplainAndAdjustExpected 12)
 
 (println (teResult))
 
